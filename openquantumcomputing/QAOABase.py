7b623dda0399c54497b6df46d43d06e72250f56c
Franz Georg Fuchs <franzgeorgfuchs@gmail.com>
2022-06-15 15:44:41 +0200

    add increase of depth

--- openquantumcomputing/QAOABase.py
+++ openquantumcomputing/QAOABase.py
@@ -12,6 +12,11 @@ class QAOABase:
         """
         self.E=None
         self.Var=None
+        self.current_depth=0 # depth at which local optimization has been done
+        self.angles_hist={}
+        self.costval={}
+        self.gamma_grid=None
+        self.beta_grid=None
 
 ################################
 # functions to be implemented:
@@ -56,9 +61,9 @@ class QAOABase:
 
         e,v = self.measurementStatistics(job, params=params)
 
-        opt_values[str(opt_iterations )] = e[0]
-        opt_angles[str(opt_iterations )] = angles
-        return -val[0]
+        #opt_values[str(opt_iterations )] = e[0]
+        #opt_angles[str(opt_iterations )] = angles
+        return -e
 
     def measurementStatistics(self, job, params):
         """
@@ -67,48 +72,53 @@ class QAOABase:
         :param job: job instance derived from BaseJob
         :return: expectation and variance
         """
-        expectations = []
-        variances = []
-
         jres=job.result()
-        print(jres)
         counts_list=jres.get_counts()
-        if not isinstance(counts_list, list):
-            jres=[job.result()]
-
-        for i, counts in enumerate(counts_list):
-            n_shots = jres.results[i].shots
+        if isinstance(counts_list, list):
+            expectations = []
+            variances = []
+            for i, counts in enumerate(counts_list):
+                n_shots = jres.results[i].shots
+                E = 0
+                E2 = 0
+                for string in counts:
+                    cost = self.cost(string, params)
+                    E += cost*counts[string]
+                    E2 += cost**2*counts[string];
+                if n_shots == 1:
+                    v = 0
+                else:
+                    E/=n_shots
+                    E2/=n_shots
+                    v = (E2-E**2)*n_shots/(n_shots-1)
+                expectations.append(E)
+                variances.append(v)
+            return expectations, variances
+        else:
+            n_shots = jres.results[0].shots
             E = 0
             E2 = 0
-            for string in counts:
+            for string in counts_list:
                 cost = self.cost(string, params)
-                E += cost*counts[string]
-                E2 += cost**2*counts[string];
+                E += cost*counts_list[string]
+                E2 += cost**2*counts_list[string];
             if n_shots == 1:
                 v = 0
             else:
                 E/=n_shots
                 E2/=n_shots
                 v = (E2-E**2)*n_shots/(n_shots-1)
-            expectations.append(E)
-            variances.append(v)
-        #else:
-        #   n_shots = jres.results.shots
-        #   E = 0
-        #   E2 = 0
-        #   for string in counts_list:
-        #       cost = self.cost(string, params)
-        #       E += cost*counts_list[string]
-        #       E2 += cost**2*counts_list[string];
-        #   if n_shots == 1:
-        #       v = 0
-        #   else:
-        #       E/=n_shots
-        #       E2/=n_shots
-        #       v = (E2-E**2)*n_shots/(n_shots-1)
-        #   expectations.append(E)
-        #   variances.append(v)
-        return expectations, variances
+            return E,v
+
+    def hist(self, angles, backend, shots, noisemodel=None, params={}):
+        depth=int(len(angles)/2)
+        circ=self.createCircuit(angles, depth, params=params)
+        if backend.configuration().local:
+            job = execute(circ, backend, shots=shots)
+        else:
+            job = start_or_retrieve_job("hist", backend, circ, options={'shots' : shots})
+        return job.result().get_counts()
+
 
     def interp(self, angles):
         """
@@ -116,14 +126,14 @@ class QAOABase:
         when going from depth p to p+1 (https://doi.org/10.1103/PhysRevX.10.021067)
         E.g. [0., 2., 3., 6., 11., 0.] becomes [2., 2.75, 4.5, 7.25, 11.]
 
-
         :param angles: angles for depth p
         :return: linear interpolation of angles for depth p+1
         """
+        depth=len(angles)
         tmp=np.zeros(len(angles)+2)
         tmp[1:-1]=angles
-        w=np.arange(0,p+1)
-        return w/p*tmp[:-1] + (p-w)/p*tmp[1:]
+        w=np.arange(0,depth+1)
+        return w/depth*tmp[:-1] + (depth-w)/depth*tmp[1:]
 
     def sample_cost_landscape(self, backend, shots, noisemodel=None, params={}, verbose=True, angles={"gamma": [0,2*np.pi,20], "beta": [0,2*np.pi,20]}):
         if verbose:
@@ -132,14 +142,14 @@ class QAOABase:
         depth=1
 
         tmp=angles["gamma"]
-        gamma_grid = np.linspace(tmp[0],tmp[1],tmp[2])
+        self.gamma_grid = np.linspace(tmp[0],tmp[1],tmp[2])
         tmp=angles["beta"]
-        beta_grid = np.linspace(tmp[0],tmp[1],tmp[2])
+        self.beta_grid = np.linspace(tmp[0],tmp[1],tmp[2])
 
         if backend.configuration().local:
             circuits=[]
-            for beta in beta_grid:
-                for gamma in gamma_grid:
+            for beta in self.beta_grid:
+                for gamma in self.gamma_grid:
                     #params['name'] = str(beta_n)+"_"+str(gamma_n)
                     circuits.append(self.createCircuit(np.array((gamma,beta)), depth, params=params))
             job = execute(circuits, backend, shots=shots)
@@ -150,10 +160,10 @@ class QAOABase:
             self.E = np.zeros((angles["beta"][2],angles["gamma"][2]))
             self.Var = np.zeros((angles["beta"][2],angles["gamma"][2]))
             b=-1
-            for beta in beta_grid:
+            for beta in self.beta_grid:
                 b+=1
                 g=-1
-                for gamma in gamma_grid:
+                for gamma in self.gamma_grid:
                     g+=1
                     params['name'] = str(b)+"_"+str(g)
                     circuit = createCircuit(np.array((gamma,beta)), depth, params=params)
@@ -161,8 +171,13 @@ class QAOABase:
                     e,v = self.measurementStatistics(job, params=params)
                     self.E[b,g] = -e[0]
                     self.Var[b,g] = -v[0]
+
+        #self.current_depth=1
         if verbose:
             print("Calculating Energy landscape done")
+
+    def get_current_deptgh(self):
+        return self.current_depth
 
     def local_opt(self, angles0, backend, shots, noisemodel=None, params={}, method='Nelder-Mead'):
         """
@@ -170,8 +185,48 @@ class QAOABase:
         :param angles0: initial guess
         """
 
-        res = minimize(self.loss, x0 = angles0, method = method,
-                       args=(backend, int(len(angles0)/2), shots, noisemodel, params),
-                       options={'xatol': 1e-2, 'fatol': 1e-1, 'disp': True})
+        depth=int(len(angles0)/2)
 
-        return res.x
+        res = minimize(self.loss, x0 = angles0, method = method,
+                       args=(backend, depth, shots, noisemodel, params),
+                       options={'xatol': 1e-2, 'fatol': 1e-1, 'disp': True})
+        return res
+
+    def increase_depth(self, backend, shots, noisemodel=None, params={}, method='Nelder-Mead'):
+
+        if self.current_depth == 0:
+            if self.E is None:
+                self.sample_cost_landscape(backend, shots, noisemodel=noisemodel, params=params, angles={"gamma": [0,2*np.pi,20], "beta": [0,2*np.pi,20]})
+            ind_Emin = np.unravel_index(np.argmin(self.E, axis=None), self.E.shape)
+            angles0=np.array((self.gamma_grid[ind_Emin[1]], self.beta_grid[ind_Emin[0]]))
+            self.angles_hist['d1_initial']=angles0
+
+            res = self.local_opt(angles0, backend, shots, noisemodel=noisemodel, params=params, method=method)
+            if not res.success:
+                raise Warning("Local optimization was not successful.", res)
+
+            self.angles_hist['d1_final']=res.x
+            self.costval['d1_final']=res.fun
+            self.current_depth=1
+        else:
+            gamma=self.angles_hist['d'+str(self.current_depth)+'_final'][::2]
+            beta=self.angles_hist['d'+str(self.current_depth)+'_final'][1::2]
+            gamma_interp=self.interp(gamma)
+            beta_interp=self.interp(beta)
+            angles0=np.zeros(2*(self.current_depth+1))
+            angles0[::2]=gamma_interp
+            angles0[1::2]=beta_interp
+            self.angles_hist['d'+str(self.current_depth+1)+'_initial']=angles0
+
+            res = self.local_opt(angles0, backend, shots, noisemodel=noisemodel, params=params, method=method)
+            if not res.success:
+                raise Warning("Local optimization was not successful.", res)
+
+            self.angles_hist['d'+str(self.current_depth+1)+'_final']=res.x
+            self.costval['d'+str(self.current_depth+1)+'_final']=res.fun
+            self.current_depth+=1
+
+
+
+
+
