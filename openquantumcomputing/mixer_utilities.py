7bde5cd14084389841b2458995b3029d52ecec79
Erlend Syljuåsen <esylju@gmail.com>
2022-06-29 09:26:20 +0200

    implement algorithm 3

--- openquantumcomputing/mixer_utilities.py
+++ openquantumcomputing/mixer_utilities.py
@@ -221,6 +221,52 @@ def get_Pauli_string(binstrings, T, symbolic=False):
 
     return pauli_str
 
+def alg3(B, T):
+    pauli_string=0
+    J=len(B)
+    for j in range(J):
+        for k in range(j+1, J):
+            if not math.isclose(T[j,k], 0, abs_tol=1e-7):
+                w,z=B[j],B[k]
+                # A1
+                if z[0]=='0' and w[0]=='0':
+                    A_n=I+Z
+                elif (z[0]=='0' and w[0]=='1') or (z[0]=='1' and w[0]=='0'):
+                    A_n=X
+                else: # z[0]==1 and w[0]==1
+                    A_n=I-Z
+                # B1
+                if z[0]=='0' and w[0]=='1':
+                    B_n=-Y
+                elif z[0]=='1' and w[0]=='0':
+                    B_n=Y
+                else: # (z[0]==0 and w[0]==0) or (z[0]==1 and w[0]==1)
+                    B_n=0.
+                for n in range(1, len(z)):
+                    x,y=z[n],w[n]
+                    # A_(n+1)
+                    if x=='0' and y=='0':
+                        A_next=TensorProduct(A_n, I+Z)
+                    elif x=='0' and y=='1':
+                        A_next=(TensorProduct(A_n, X)+TensorProduct(B_n, Y))
+                    elif x=='1' and y=='0':
+                        A_next=(TensorProduct(A_n, X)-TensorProduct(B_n, Y))
+                    else: # x==1 and y==1
+                        A_next=TensorProduct(A_n, I-Z)
+                    # B_(n+1)
+                    if x=='0' and y=='0':
+                        B_next=(TensorProduct(B_n, I+Z))
+                    elif x=='0' and y=='1':
+                        B_next=(TensorProduct(B_n, X)-TensorProduct(A_n, Y))
+                    elif x=='1' and y=='0':
+                        B_next=(TensorProduct(B_n, X)+TensorProduct(A_n, Y))
+                    else: # x==1 and y==1
+                        B_next=(TensorProduct(B_n, I-Z))
+                    A_n=0.5*A_next
+                    B_n=0.5*B_next
+                pauli_string+=A_n*T[j,k]
+    return pauli_string
+
 def simplifyH(H):
     for i in range(10):
         H = H.expand(tensorproduct=True)
