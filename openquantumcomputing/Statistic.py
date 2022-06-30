b450adc0d7444b3a931da57da6d18c8d3ac314a6
Franz Georg Fuchs <franzgeorgfuchs@gmail.com>
2022-06-30 12:46:00 +0200

    calculate statistics recursively

---
+++ openquantumcomputing/Statistic.py
@@ -0,0 +1,22 @@
+class Statistic:
+    """
+    See: https://fanf2.user.srcf.net/hermes/doc/antiforgery/stats.pdf
+    """
+
+    def __init__(self):
+        self.W=0
+        self.E=0
+        self.S=0
+
+    def add_sample(self, value, weight):
+        self.W+=weight
+        tmp_E=self.E
+        self.E+=weight/self.W*(value-self.E)
+        self.S+=weight*(value-tmp_E)*(value-self.E)
+
+    def get_E(self):
+        return self.E
+
+    def get_Variance(self):
+        return self.S/(self.W-1)
+
